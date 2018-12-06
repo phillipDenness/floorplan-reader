eedc0a27ce9ef53e5d8865a23c727434c1cc367e
Phillip Denness <dennessphillip@gmail.com>
2018-12-06 23:51:30 +0000

    enable cors

--- src/main/java/com/phillip/denness/fps/controller/FloorplanController.java
+++ src/main/java/com/phillip/denness/fps/controller/FloorplanController.java
@@ -5,10 +5,7 @@ import com.phillip.denness.fps.domain.Floorplan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/floor-plan")
@@ -17,6 +14,7 @@ public class FloorplanController {
     @Autowired
     private FloorplanService floorplanService;
 
+    @CrossOrigin
     @PostMapping(value = "")
     public ResponseEntity<Floorplan> postSearchTerms(@RequestBody Floorplan floorplan) {
         floorplanService.processImage(floorplan);
